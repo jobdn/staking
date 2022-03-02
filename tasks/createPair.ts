7cd0ca1664ba6a66f02168ca14fd825df8203664
Danila Pisarev <jobdan@bk.ru>
2022-03-02 08:32:39 +0300

    Added task to create the pair

--- tasks/createPair.ts
+++ tasks/createPair.ts
@@ -10,9 +10,24 @@ task("createPair", "Create the pair")
       "IUniswapV2Factory",
       FACTORY_ADDRESS
     );
+    // const pairAddress = (
+    //   await Factory.createPair(taskArgs.token1, taskArgs.token2)
+    // ).wait();
+
+    const [ owner ] = await hre.ethers.getSigners();
+
+    const FactoryWithSigner = Factory.connect(owner);
+
     const pairAddress = (
-      await Factory.createPair(taskArgs.token1, taskArgs.token2)
-    ).wait();
+      await FactoryWithSigner.createPair(taskArgs.token1, taskArgs.token2)
+    )
+
     console.log(`Pair address: ${pairAddress}`);
     console.log(pairAddress);
   });
+
+  // pair: 0x4EE38eaf103288db9A17fa1D027c1d33F6653549
+
+  // pair: 0x79e2F77df7204746F4E175D7ba7c135B3EAF5399
+  // token: 0x9603216F56A8AD58d51c87Fe9ECb96dE905814C1
+  // me: 0x5c0D12FCFEDBC5f19c3e4a0E4093EcCCd7459aCF
